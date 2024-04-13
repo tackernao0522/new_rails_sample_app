9beb39606b616cfd2fc75d104efe7a02ba2f6df1
takaki <takaki55730317@gmail.com>
2024-04-13 12:55:02 +0900

    add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::Base
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base # rubocop:disable Style/Documentation
+  def hello
+    render html: 'hello, world!'
+  end
 end
