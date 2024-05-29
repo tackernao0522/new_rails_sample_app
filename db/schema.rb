f59d3cbe4c59c6dca640dda998e1aa9241d28187
takaki <takaki55730317@gmail.com>
2024-05-29 20:07:23 +0900

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_240_513_093_125) do
+ActiveRecord::Schema[7.0].define(version: 20_240_529_085_532) do
   create_table 'users', force: :cascade do |t|
-    t.string 'name'
+    t.string "name" # rubocop:disable Style/StringLiterals
     t.string 'email'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.string 'password_digest'
     t.string 'remember_digest'
     t.boolean 'admin', default: false
-    t.string 'activation_digest'
+    t.string "activation_digest" # rubocop:disable Style/StringLiterals
     t.boolean 'activated', default: false
     t.datetime 'activated_at'
+    t.string 'reset_digest'
+    t.datetime 'reset_sent_at'
     t.index ['email'], name: 'index_users_on_email', unique: true
   end
 end
